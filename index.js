d19f219dae64336486fe07b980fde751aae4f0c2
laurpe <laura.perttila@edu.bc.fi>
2022-04-21 18:20:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+
+app.get("/api/plants", function (req, res) {
+    res.send({ id: "1245" });
+});
+
+app.listen(3001);
