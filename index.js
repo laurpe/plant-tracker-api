5946525db09bff79f85cc0234f98d76091604f63
laurpe <laura.perttila@edu.bc.fi>
2022-04-28 17:43:02 +0300

    update plant lastwatered

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import Plant from "./models/plant.js";
 import mongoose from "mongoose";
 import { faker } from "@faker-js/faker";
 import dotenv from "dotenv";
-import { response } from "express";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const url = process.env.MONGODB_URI;
 
@@ -30,6 +31,23 @@ app.get("/", function (req, res) {
 app.get("/api/plants", async (req, res) => {
     const plants = await Plant.find({});
     res.json(plants);
+});
+
+app.get("/api/plants/:id", async (req, res) => {
+    const plant = await Plant.findById(req.params.id);
+    res.json(plant);
+});
+
+app.put("/api/plants/:id", async (req, res) => {
+    const { lastWatered } = req.body;
+    const plant = await Plant.findByIdAndUpdate(
+        req.params.id,
+        { lastWatered: lastWatered },
+        {
+            new: true,
+        }
+    );
+    res.json(plant);
 });
 
 app.post("/api/plants", async (req, res) => {
